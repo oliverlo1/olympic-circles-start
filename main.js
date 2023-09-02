a355c5d2fad10d743bbd36b6fdd5e785de761daf
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2023-09-02 17:34:43 -0600

    demo

--- main.js
+++ main.js
@@ -1,18 +1,49 @@
-// Canvas Template
+// Olympic Rings Start Code
 
 // Canvas Setup
 let cnv = document.getElementById("my-canvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 600;
-cnv.height = 400;
+cnv.width = 440;
+cnv.height = 250;
 
 // Call draw function once all page resources have loaded
 window.addEventListener("load", draw);
 
 function draw() {
-  ctx.fillStyle = "green";
-  ctx.fillRect(50, 100, 200, 80);
+  // Background
+  ctx.fillStyle = "#eee";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-  // Redraw
-  requestAnimationFrame(draw);
+  // Circle Line Width
+  ctx.lineWidth = 8;
+
+  // Blue Ring - Center: (100, 100) Radius: 50
+  ctx.strokeStyle = "blue";
+  ctx.beginPath();
+  ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  // Yellow Ring - Center: (160, 150) Radius: 50
+  ctx.strokeStyle = "yellow";
+  ctx.beginPath();
+  ctx.arc(160, 150, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  // Black Ring - Center: (220, 100) Radius: 50
+  ctx.strokeStyle = "black";
+  ctx.beginPath();
+  ctx.arc(220, 100, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  // Blue Ring - Center: (280, 150) Radius: 50
+  ctx.strokeStyle = "green";
+  ctx.beginPath();
+  ctx.arc(280, 150, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  // Red Ring - Center: (340, 100) Radius: 50
+  ctx.strokeStyle = "red";
+  ctx.beginPath();
+  ctx.arc(340, 100, 50, 0, 2 * Math.PI);
+  ctx.stroke();
 }
